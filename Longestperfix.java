b35a6ce6b92028f8851af90af6a6444c0cb1bef3
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-11 15:56:40 +0530

    Create Longestperfix.java

---
+++ Longestperfix.java
@@ -0,0 +1,32 @@
+# array
+import java.io.*;
+import java.util.*;
+ public class Longestperfix
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    String x = sc.nextLine();
+    String y = sc.nextLine();
+    int len=x.length();
+    if(len>y.length())
+    {
+      len=y.length();
+    }
+    
+    StringBuilder a = new StringBuilder();
+    for(int i=0;i<len;i++)
+    {
+      if(x.charAt(i)==y.charAt(i))
+      {
+        a.append(x.charAt(i));
+      }
+      else
+      {
+        break;
+      }
+    }       
+    System.out.println(a.toString());
+  }
+  
+}
